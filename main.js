e359dd98c79ececd112d53e792c29904d4aceb30
taejin <taejin5314@gmail.com>
2020-09-28 20:20:44 -0400

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark')
+    } else {
+        navbar.classList.remove('navbar--dark')
+    }
+})
